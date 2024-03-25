ce9eaec218a3b1689bd33b1aadd80d90b9a834b6
hugo566 <hugo56.lg@gmail.com>
2024-03-25 20:26:16 +0100

    Ajout section realisations

---
+++ src/components/modal.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './modal.scss';
+
+export default function Modal(props) {
+    const overlayClasses = ['overlay', props.show === "show" ? 'show' : ''];
+    const modalClasses = ['modal', props.show === "show" ? `show` : ''];
+
+    return (
+        <div className={overlayClasses.join(' ')} onClick={props.btnClose}>
+            <div className={modalClasses.join(' ')} onClick={(e) => e.stopPropagation()}>
+                <h1>{props.titre}</h1>
+                <i onClick={props.btnClose} className="fa-solid fa-xmark"></i>
+                {props.children}
+            </div>
+        </div>
+    );
+}
--- src/sections/projectSection.jsx
+++ src/sections/projectSection.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './projectSection.scss';
-import { realisations } from '../../public/realisations.json';
+import { realisationsBts, realisationsOC } from '../../public/realisations.json';
+import Modal from '../components/modal';
 
 export default function ProjectSection() {
-    function getFirstSentence(text) {
+
+    const [modalVisible, setModalVisible] = useState(false)
+    const handleOpenModal = () => {
+        setModalVisible(true);
+    }
+
+    const handleCloseModal = () => {
+        setModalVisible(false);
+    }
+
+    const getFirstSentence = (text) => {
         if (!text) {
             return '';
         }
@@ -18,20 +29,81 @@ export default function ProjectSection() {
     return (
         <section id='project' className="section project">
             <h1>Réalisations</h1>
+            <h2>BTS SIO</h2>
             <div className="containerProject">
-                {realisations.map((realisation) => (
+                {realisationsBts.map((realisation) => (
                     <div className="containerRealisation">
                         <div className="img">
                             <img src={realisation.image} alt={realisation.title} srcset="" />
                             <div className="containHover">
                                 <p>{realisation.date}</p>
                                 <p>{getFirstSentence(realisation.mission)}</p>
-                                <i class="fa-solid fa-plus"></i>
+                                <i onClick={handleOpenModal} className="fa-solid fa-plus"></i>
+                                {
+                                    modalVisible &&
+                                    <Modal show="show" btnClose={handleCloseModal} titre={realisation.title}>
+                                        <div className="containModal">
+                                            <p><b>Mission:</b> {realisation.mission}</p>
+                                        </div>
+                                        <div className="containModal">
+                                            <p><b>Contexte:</b> {realisation.context}</p>
+                                        </div>
+                                        <div className="containModal">
+                                            <p><b>Compétences:</b> {realisation.skills}</p>
+                                        </div>
+                                        <div className="containModal">
+                                            <a href={realisation.ressource} target="_blank" rel="noopener noreferrer">Ressources</a> - <a href="../public/images/fichedecompetences.pdf" target="_blank" rel="noopener noreferrer">Fiche de compétences</a>
+                                        </div>
+                                    </Modal>
+                                }
                             </div>
                         </div>
                         <div className="details">
                             <h3>{realisation.title}</h3>
                             <p>{realisation.semestre}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <h2>OpenclassRooms</h2>
+            <div className="containerProject">
+                {realisationsOC.map((realisation) => (
+                    <div className="containerRealisation">
+                        <div className="img">
+                            <img src={realisation.image} alt={realisation.title} srcset="" />
+                            <div className="containHover">
+                                <p>{realisation.date}</p>
+                                <p>{getFirstSentence(realisation.mission)}</p>
+                                <i onClick={handleOpenModal} className="fa-solid fa-plus"></i>
+                                {
+                                    modalVisible &&
+                                    <Modal show="show" btnClose={handleCloseModal} titre={realisation.title}>
+                                        <div className="containModal">
+                                            <p><b>Mission:</b> {realisation.mission}</p>
+                                        </div>
+                                        <div className="containModal">
+                                            <p><b>Contexte:</b> {realisation.context}</p>
+                                        </div>
+                                        <div className="containModal">
+                                            <p><b>Techonologies :</b> {realisation.techno}</p>
+                                        </div>
+                                        <div className="containModal">
+                                            <p><b>Compétences:</b> </p>
+                                            {Object.values(realisation.skills).map((skill, index) => (
+                                                <p key={index}>{skill}</p>
+                                            ))}
+                                        </div>
+                                        <div className="containModal">
+                                            <a href={realisation.ressource} target="_blank" rel="noopener noreferrer">Ressources</a>
+                                        </div>
+                                    </Modal>
+                                }
+                            </div>
+                        </div>
+                        <div className="details">
+                            <h3>{realisation.title}</h3>
+                            <p>{realisation.techno}</p>
                         </div>
                     </div>
                 ))}
