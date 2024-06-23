d6814461d56c14cead7e9a105c67dd6e1b66b9cf
Hugo Le Galle <hugo@Ordinateur-portable-de-Hugo.local>
2024-06-23 16:41:26 +0200

    commit

--- src/sections/aboutSection.jsx
+++ src/sections/aboutSection.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './aboutSection.scss';
+import { calculerAge } from '../utils/function';
 
 export default function AboutSection() {
+
+    const age = calculerAge(2002, 7)
 
     return (
         <section id='about' className="section about">
@@ -9,7 +12,7 @@ export default function AboutSection() {
             <div className="containerAbout">
                 <img src="./images/portrait.png" alt="" srcSet="" />
                 <div className="containerText">
-                    <p>Je m'appelle Hugo, j'ai  21 ans et je suis passionné par le développement web. Actuellement à la recherche d'une alternance dans ce domaine, je suis motivé et prêt à m'investir pleinement dans une entreprise qui saura me permettre de mettre en pratique mes compétences et d'en acquérir de nouvelles</p>
+                    <p>Je m'appelle Hugo, j'ai  {age} ans et je suis passionné par le développement web. Actuellement à la recherche d'une alternance dans ce domaine, je suis motivé et prêt à m'investir pleinement dans une entreprise qui saura me permettre de mettre en pratique mes compétences et d'en acquérir de nouvelles</p>
                 </div>
             </div>
         </section>
