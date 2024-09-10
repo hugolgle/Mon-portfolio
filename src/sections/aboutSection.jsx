b6ef6dd75aae14e14ef46c715970aa1499d47822
Hugo Le Galle <hugo@Ordinateur-portable-de-Hugo.local>
2024-09-10 21:45:01 +0200

    commit

--- src/sections/aboutSection.jsx
+++ src/sections/aboutSection.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import './aboutSection.scss';
-import { calculerAge } from '../utils/function';
+import React from "react";
+import "./aboutSection.scss";
+import { calculerAge } from "../utils/function";
 
 export default function AboutSection() {
+  const age = calculerAge(2002, 7);
 
-    const age = calculerAge(2002, 7)
-
-    return (
-        <section id='about' className="section about">
-            <h1>Présentation</h1>
-            <div className="containerAbout">
-                <img src="./images/portrait.png" />
-                <div className="containerText">
-                    <p>Je m'appelle Hugo, j'ai {age} ans et je suis passionné par le développement web. Actuellement étudiant à l'école MyDigitalSchool à Vannes, je combine mes études avec une alternance chez Atempo. Cette expérience professionnelle enrichissante me permet de mettre en pratique mes connaissances et de travailler sur des projets concrets, tout en continuant à approfondir mes compétences techniques et créatives dans le domaine du développement web.</p>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="about" className="section about">
+      <h1>Présentation</h1>
+      <div className="containerAbout">
+        <img src="./images/portrait.png" />
+        <div className="containerText">
+          <p>
+            Je m'appelle Hugo, j'ai {age} ans et je suis passionné par le
+            développement web. Actuellement étudiant à l'école MyDigitalSchool à
+            Vannes, je combine mes études avec une alternance chez Atempo. Cette
+            expérience professionnelle enrichissante me permet de mettre en
+            pratique mes connaissances et de travailler sur des projets
+            concrets, tout en continuant à approfondir mes compétences
+            techniques et créatives dans le domaine du développement web.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/sections/skillsSection.jsx
+++ src/sections/skillsSection.jsx
@@ -1,42 +1,128 @@
-import React from 'react';
-import './skillsSection.scss';
+import "./skillsSection.scss";
 
-import BoxSkills from '../components/boxSkills';
-import Button from '../components/button';
+import BoxSkills from "../components/boxSkills";
+import Button from "../components/button";
 
 export default function SkillsSection() {
-    return (
-        <section id='skills' className="section skills">
-            <h1>Compétences</h1>
+  return (
+    <section id="skills" className="section skills">
+      <h1>Compétences</h1>
 
-            <div className="containerSkills">
-                <BoxSkills title="Langages">
-                    <img src="./images/iconSkills/iconJS.png" alt="" srcset="" title='JavaScript' className='iconSkills' />
-                    <img src="./images/iconSkills/iconTS.png" alt="" srcset="" title='TypeScript' className='iconSkills' />
-                    <img src="./images/iconSkills/iconMySQL.png" alt="" srcset="" title='MySQL' className='iconSkills' />
-                    <img src="./images/iconSkills/iconPHP.png" alt="" srcset="" title='PHP' className='iconSkills' />
-                    <img src="./images/iconSkills/iconPython.png" alt="" srcset="" title='Python' className='iconSkills' />
-                </BoxSkills>
-                <BoxSkills title="Technologies">
-                    <img src="./images/iconSkills/iconWordpress.png" alt="" srcset="" title='WordPess' className='iconSkills' />
-                    <img src="./images/iconSkills/iconReact.png" alt="" srcset="" title='React' className='iconSkills' />
-                    <img src="./images/iconSkills/iconNode.png" alt="" srcset="" title='NodeJS' className='iconSkills' />
-                    <img src="./images/iconSkills/iconTailwind.png" alt="" srcset="" title='Tailwind' className='iconSkills' />
-                    <img src="./images/iconSkills/iconSASS.png" alt="" srcset="" title='SASS' className='iconSkills' />
-                    <img src="./images/iconSkills/iconBootstrap.png" alt="" srcset="" title='Bootstrap' className='iconSkills' />
-                </BoxSkills>
-                <BoxSkills title="Outils">
-                    <img src="./images/iconSkills/iconFigma.png" alt="" srcset="" title='Figma' className='iconSkills' />
-                    <img src="./images/iconSkills/iconVSCode.png" alt="" srcset="" title='Visual Studio Code' className='iconSkills' />
-                    <img src="./images/iconSkills/iconGit.png" alt="" srcset="" title='Git' className='iconSkills' />
-                    <img src="./images/iconSkills/iconGitHub.png" alt="" srcset="" title='GitHub' className='iconSkills' />
-                </BoxSkills>
-                <BoxSkills title="Conception" class='concept'>
-                    <img src="./images/iconSkills/iconWindows.png" alt="" srcset="" title='Windows' className='iconSkills' />
-                    <img src="./images/iconSkills/iconLinux.png" alt="" srcset="" title='Linux' className='iconSkills' />
-                </BoxSkills>
-            </div>
-            <a href="./images/CV.pdf" target="_blank" rel="noopener noreferrer"><Button title="Voir mon CV" /></a>
-        </section >
-    );
+      <div className="containerSkills">
+        <BoxSkills title="Langages">
+          <img
+            src="./images/iconSkills/iconJS.png"
+            alt=""
+            title="JavaScript"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconTS.png"
+            alt=""
+            title="TypeScript"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconMySQL.png"
+            alt=""
+            title="MySQL"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconPHP.png"
+            alt=""
+            title="PHP"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconPython.png"
+            alt=""
+            title="Python"
+            className="iconSkills"
+          />
+        </BoxSkills>
+        <BoxSkills title="Technologies">
+          <img
+            src="./images/iconSkills/iconWordpress.png"
+            alt=""
+            title="WordPess"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconReact.png"
+            alt=""
+            title="React"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconNode.png"
+            alt=""
+            title="NodeJS"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconTailwind.png"
+            alt=""
+            title="Tailwind"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconSASS.png"
+            alt=""
+            title="SASS"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconBootstrap.png"
+            alt=""
+            title="Bootstrap"
+            className="iconSkills"
+          />
+        </BoxSkills>
+        <BoxSkills title="Outils">
+          <img
+            src="./images/iconSkills/iconFigma.png"
+            alt=""
+            title="Figma"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconVSCode.png"
+            alt=""
+            title="Visual Studio Code"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconGit.png"
+            alt=""
+            title="Git"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconGitHub.png"
+            alt=""
+            title="GitHub"
+            className="iconSkills"
+          />
+        </BoxSkills>
+        <BoxSkills title="Conception">
+          <img
+            src="./images/iconSkills/iconWindows.png"
+            alt=""
+            title="Windows"
+            className="iconSkills"
+          />
+          <img
+            src="./images/iconSkills/iconLinux.png"
+            alt=""
+            title="Linux"
+            className="iconSkills"
+          />
+        </BoxSkills>
+      </div>
+      <a href="./images/CV.pdf" target="_blank" rel="noopener noreferrer">
+        <Button title="Voir mon CV" />
+      </a>
+    </section>
+  );
 }
